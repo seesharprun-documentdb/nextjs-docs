0c0d93eaf4f3bc66a83ecc046940d66590613a3c
seesharprun <sidandrews@microsoft.com>
2025-09-25 13:48:40 -0400

    Update workflow to use base paths

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const basePath = process.env.NEXT_BASE_PATH ?? undefined;
+
 const nextConfig: NextConfig = {
-  output: 'export'
+  output: 'export',
+  basePath: basePath ? `/${basePath}` : undefined,
+  assetPrefix: basePath ? `${basePath}/` : undefined,
+  // If using images, add: images: { unoptimized: true }
 };
 
 export default nextConfig;
